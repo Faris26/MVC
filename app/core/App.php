5e5a43d56e82fe3d795b6e4dc03ba8ea2ee78d03
Faris26 <farisikhlasulhaq@yahoo.co.id>
2021-02-15 08:11:56 +0700

    Rev

--- app/core/App.php
+++ app/core/App.php
@@ -1,13 +1,24 @@
 <?php 
 
 class App {
+
+ /*    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = []; */
     public function __construct(){
-        var_dump($_GET);
+           /*  $url=$this->parseURL();
+            if(file_exists('../app/controllers'. $url[0].'.php')){
+                $this->controller = $url[0];
+                var_dump($url);
+            } */
+            echo 'ok';
     }
-    public function parseURL(){
+   /*  public function parseURL(){
         if(isset($_GET['url'])){
             $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url) ;
             return $url;
         }
-    }
+    } */
 }
