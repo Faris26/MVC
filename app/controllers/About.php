601c50ef37eb22bb4cb23ab32c514da75a96abdd
Faris26 <farisikhlasulhaq@yahoo.co.id>
2021-02-16 06:55:25 +0700

    Rev Update

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php  
+
+class About {
+
+    public function index($nama, $pekerjaan)
+    {
+        echo "Halo, Nama saya $nama, saya adalah Seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo "About/page";
+    }
+}
+?>
